6ec51d884cd90023faea690e10ec26d0471c58f4
Matvey Tolpeev <vova-bulgakov@mail.ru>
2023-02-12 22:50:00 +0500

    complete db model for market

--- backend/app_webshop/market/admin.py
+++ backend/app_webshop/market/admin.py
@@ -4,4 +4,35 @@ from market.models import Provider, Product, Order, OrderProduct, Consumer, Stor
 class ProviderAdmin(admin.ModelAdmin):
     pass
 
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class ConsumerAdmin(admin.ModelAdmin):
+    pass
+
+
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(OrderProduct, OrderProductAdmin)
+admin.site.register(Consumer, ConsumerAdmin)
+admin.site.register(Order, OrderAdmin)
